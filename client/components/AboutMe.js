75131dd740704f12520a4f7d92ce9f9231ffd502
Rhea Wu <rheawu1212@gmail.com>
2023-03-27 14:16:00 -0400

    modify contact.js & aboutme.js

--- client/components/AboutMe.js
+++ client/components/AboutMe.js
@@ -3,7 +3,14 @@ import React from 'react';
 const AboutMe = () => {
     return (
         <div>
-            <h2>about me here...</h2>
+            <h2>Something you might want to know </h2>
+            <p className='intro-swe'>
+                I'm a full-stack engineer, with expertise in MERN, SQL, GraphQL, Typescript end-to-end application. I like to give <a href='https://www.linkedin.com/posts/rheawu-tech_i-was-recently-invited-to-speak-on-load-balancers-activity-7018955111016103936-8k89?utm_source=share&utm_medium=member_desktop'>public speech</a> and co-founded Typescript <a href='https://medium.com/@lightql/how-to-implement-client-side-caching-for-graphql-optimising-for-run-time-speed-cache-persistence-67cf72319163'>application</a> for GraphQL.
+            </p>
+            <h2>But that's not everything about me </h2>
+            <p className='intro-self'>
+                I'm a cat mom, Muay Thai practitioner, food lover, vintage collector, and more to explore 
+            </p>
         </div>
     )
 };
--- client/components/Contact.js
+++ client/components/Contact.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            <h3>email:xxx</h3><h3>Linkedin:xxx</h3><h3>github:xxx</h3>
+            <h3>rheawu1212@gmail.com</h3>
+            <h3><a href='https://www.linkedin.com/in/rheawu-tech/'>LinkedIn</a></h3>
+            <h3><a href='https://github.com/RheaWu1212'>Github</a></h3>
         </div>
     )
 }
